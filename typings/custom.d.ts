ae725ad3c0029242b0081847029f67de73530150
Hongbo LU <hongbo.lu@kellydeli.com>
2017-03-01 15:19:00 +0100

    chore(typings): added custom typing

---
+++ typings/custom.d.ts
@@ -0,0 +1,10 @@
+declare namespace LogEx {
+  interface ILogService {
+    getInstance(id: string, override?: boolean, useTemplate?: boolean, colorCss?: string): ILogService;
+    debug(...msg: any[]): void;
+    info(...msg: any[]): void;
+    log(...msg: any[]): void;
+    warn(...msg: any[]): void;
+    error(...msg: any[]): void;
+  }
+}
